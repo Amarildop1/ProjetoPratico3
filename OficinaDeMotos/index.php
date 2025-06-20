7130345da26cf33e754727ffd03952e400a9ee7f
Amarildo <amarildop1dev@gmail.com>
2025-06-19 23:54:08 -0300

    Adicionando arquivos iniciais

---
+++ OficinaDeMotos/index.php
@@ -0,0 +1,8 @@
+<?php include('includes/header.php'); ?>
+
+    <section>
+        <h1>Bem-vindo à Motos e Motores</h1>
+        <p>Somos especializados em peças e manutenção de motos antigas. Com mais de 20 anos no mercado, oferecemos qualidade e tradição para apaixonados por duas rodas.</p>
+    </section>
+
+<?php include('includes/footer.php'); ?>
