14bb2635730d0890c45c0dd51705ae65bfa7e1fd
Amarildo <amarildop1dev@gmail.com>
2025-06-24 22:15:56 -0300

    Ajustado as imagens da página serviços

--- OficinaDeMotos/servicos.php
+++ OficinaDeMotos/servicos.php
@@ -3,7 +3,7 @@
 <h1>Serviços de Manutenção</h1>
 <section class="servicos-cards">
     <div class="card-servico">
-        <img src="imagens/oleo.jpg" alt="Troca de Óleo">
+        <img src="imagens/mecanica.png" alt="Mecânica em Geral">
         <h3>Mecânica em Geral</h3>
         <p>Solucionamos seus problemas e fazemos reparos duradouros.</p>
     </div>
@@ -13,33 +13,33 @@
         <p>Reconstruímos peças originais com acabamento fiel ao modelo de fábrica.</p>
     </div>
     <div class="card-servico">
-        <img src="imagens/eletrica.jpg" alt="Elétrica">
+        <img src="imagens/eletrica2.jpg" alt="Elétrica">
         <h3>Elétrica</h3>
         <p>Diagnóstico e reparo completo da parte elétrica e iluminação clássica.</p>
     </div>
     <div class="card-servico">
-        <img src="imagens/oleo.jpg" alt="Troca de Óleo">
+        <img src="imagens/oleo.png" alt="Troca de Óleo">
         <h3>Troca de Óleo</h3>
         <p>Utilizamos óleo recomendado para motores clássicos com troca de filtro incluso.</p>
     </div>
 
     <div class="card-servico">
-        <img src="imagens/eletrica.jpg" alt="Elétrica">
+        <img src="imagens/carburador.jpg" alt="Limpeza de Carburador">
         <h3> Limpeza de Carburador </h3>
         <p>Reparo e limpeza completa de Carburadores.</p>
     </div>
     <div class="card-servico">
-        <img src="imagens/eletrica.jpg" alt="Elétrica">
+        <img src="imagens/moto1.jpg" alt="Troca de Peças">
         <h3> Troca de Peças </h3>
         <p>Troca de peças danificadas e irrecuperáveis.</p>
     </div>
     <div class="card-servico">
-        <img src="imagens/eletrica.jpg" alt="Elétrica">
+        <img src="imagens/bateria.jpg" alt="Substituição de Bateria">
         <h3> Substituição de Bateria </h3>
         <p>Substituição da bateria e os conectores relacionados.</p>
     </div>
     <div class="card-servico">
-        <img src="imagens/eletrica.jpg" alt="Elétrica">
+        <img src="imagens/moto1.jpg" alt="Manutenção preventiva">
         <h3> Manutenção preventiva </h3>
         <p>Vistoria e manutenção preventiva para aumentar a vida útil das suas peças.</p>
     </div>
